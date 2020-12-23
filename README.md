2929e6bdd089283e2ef38d89b782d3531c5df416
Nillkizz <kotovvsan@yandex.ru>
2020-12-24 02:57:53 +0300

    Added code and written MD

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # django_template_tags
 Custom template tags for django.
+
+> Adds tags such, as: "multiply", "range"  
+> Before use needs load {% load common_tags %}
+
+For install:  
+Add "templateteags" dit to your application and register "common_tags" in the settings to "TEMPLATES" > "libraries":
+       
+       TEMPLATES = [
+            {
+                'BACKEND': '...',
+                'DIRS': [...],
+                'APP_DIRS': ...,
+                'OPTIONS': {...}
+                ],
+                'libraries': {
+                    'common_tags': 'main.templatetags.common_tags',
+                    }
+                },
+            },
+        ]
+
+       
+
+Repeat content by number of times:
+
+    {% multiply <num: int> %}  
+        <Content>  
+    {% endmultiply %}
+
+
+"range" it is generated list from range and passed to template tag "cycle" and might be used like "cycle" tag with number list, or in multiply like cycle number generator.  
+Examples syntax:
+
+    {% range <end: int> %}
+    {% range <start: int> <end: int> %}
+    {% range <start: int> <end: int> <step: int> %}
+    {% range <start: int> <end: int> <step: int> as <var> %}
+    {% range <start: int> <end: int> <step: int> as <var> <silent> %}
+  
---
+++ templatetags/common_tags.py
@@ -0,0 +1,60 @@
+from django.template import Library, Node, NodeList
+from django.template.defaulttags import CycleNode
+from django.template.base import TemplateSyntaxError
+from django.utils.safestring import SafeString
+
+register = Library()
+
+
+class Multiplier(Node):
+    def __init__(self, nodelist, ratio):
+        self.nodelist = nodelist
+        self.ratio = ratio
+
+    def render(self, context):
+        content = ''
+        for _ in range(self.ratio):
+            content += self.nodelist.render(context)
+        return content
+
+
+@register.tag
+def multiply(parser, token):
+    bits = token.split_contents()
+    ratio = int(bits[1])
+    nodelist = parser.parse(('endmultiply', None))
+    parser.next_token()
+    return Multiplier(nodelist, ratio)
+
+
+@register.tag(name='range')
+def do_range(parser, token):
+    args = token.split_contents()
+
+    as_form = False
+    if len(args) > 3:
+        # {% cycle ... as foo [silent] %} case.
+        if args[-3] == "as":
+            if args[-1] != "silent":
+                raise TemplateSyntaxError("Only 'silent' flag is allowed after range cycle's name, not '%s'." % args[-1])
+            as_form = True
+            silent = True
+            args = args[:-1]
+        elif args[-2] == "as":
+            as_form = True
+            silent = False
+
+    if as_form:
+        name = args[-1]
+        values = [parser.compile_filter(arg) for arg in args[1:-2]]
+        node = CycleNode(values, name, silent=silent)
+        if not hasattr(parser, '_named_cycle_nodes'):
+            parser._named_cycle_nodes = {}
+        parser._named_cycle_nodes[name] = node
+    elif len(args) < 5:
+        values = [parser.compile_filter(str(arg)) for arg in range(*[int(arg) for arg in args[1:]])]
+        node = CycleNode(values)
+    else:
+        raise TemplateSyntaxError('Range allowed only 3 args: (start, stop, step)')
+
+    return node
